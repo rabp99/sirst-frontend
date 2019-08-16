e935ba8d1bb3a95ffa58c7fb73d850e0825f8ad9
rabp_ <rabp_@192.168.1.4>
2019-08-15 19:55:08 -0500

    Primer commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sirstFrontendApp
+ * @description
+ * # sirstFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sirstFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sirstFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sirstFrontendApp
+ */
+angular.module('sirstFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
